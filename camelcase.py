57cf8d64c23f90d21c2723e8d34549f838e88a61
kav04 <51361047+kav04@users.noreply.github.com>
2019-07-24 20:38:13 +0530

    Update camelcase.py

--- camelcase.py
+++ camelcase.py
@@ -2,7 +2,7 @@ s=input()
 print(s[0].upper(),end="")
 for i in range(1,len(s)):
     if(s[i]!=" "):
-        print(s[i],end="")
+        print(s[i].lower(),end="")
     else:
         print(s[i+1].upper(),end="")
         
