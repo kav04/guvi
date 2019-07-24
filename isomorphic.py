ff6f358d4d58ab77998408410b0a0166ef6087e0
kav04 <51361047+kav04@users.noreply.github.com>
2019-07-24 20:31:35 +0530

    Create isomorphic.py

---
+++ isomorphic.py
@@ -0,0 +1,10 @@
+s=input()
+count=0
+print(s[0].upper(),end="")
+for i in range(1,len(s)):
+    if(s[i]!=" "):
+        print(s[i],end="")
+    else:
+        print(s[i+1].upper(),end="")
+    
+    
