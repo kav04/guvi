ee7ced4eeb2c09e6868f7f8b916516a1e6b07948
kav04 <51361047+kav04@users.noreply.github.com>
2019-07-26 12:37:44 +0530

    Create tenth.py

---
+++ tenth.py
@@ -0,0 +1,12 @@
+n,q=map(int,input().split())
+m=list(map(int,input().split()))
+count=0
+for i in range(len(m)):
+  for j in range(i+1,len(m)):
+    if(m[i]+m[j]==q):
+        count=1
+        break
+if(count):
+   print("YES")
+else:
+   print("NO")
