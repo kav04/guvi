1eadbb29194b8d81bef0deeeb2d2ae05465fabec
kav04 <51361047+kav04@users.noreply.github.com>
2019-07-28 20:07:11 +0530

    Create once.py

---
+++ once.py
@@ -0,0 +1,10 @@
+np=int(input())
+m=list(map(int,input().split(" ")))
+d={}
+for key in m: 
+    d[key]=m.count(key) 
+l=[]
+for key,values in d.items():
+    if(d[key]==1): 
+        print(key) 
+        break
