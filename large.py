0c7976431cd35f6dbe240759f6b30da2ff6971bd
kav04 <51361047+kav04@users.noreply.github.com>
2019-07-28 16:39:30 +0530

    Update large.py

--- large.py
+++ large.py
@@ -2,13 +2,12 @@ def func():
     n=int(input())
     lst=[] 
     for x in range(0,n):
-        num=(input())
+        num=input()
         lst.append(num)
     lst.sort()
     lst.reverse()
     return lst
 op=func()
-res=""
-for i in range(0,len(op)):
-    res=res+op[i] 
-print(int(res))
+print("".join(op))
+
+
