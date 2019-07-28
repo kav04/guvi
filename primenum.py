20e7860562cf312884719ec48d731b8eac2bf959
kav04 <51361047+kav04@users.noreply.github.com>
2019-07-28 20:10:44 +0530

    Create primenum.py

---
+++ primenum.py
@@ -0,0 +1,15 @@
+a=int(input())
+s=0
+for i in range(2,a): 
+    flag=1 
+    for j in range(2,i): 
+        if(i%j==0): 
+            flag=0
+            break 
+    if(flag): 
+        k=str(i) 
+        l=[]
+        for m in range(len(k)): 
+            l.append(k[m]) 
+        if(l[-1]=='3'): 
+            s=s+iprint(s)
