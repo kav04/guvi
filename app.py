cabeeea08693102906211b45d9fbf40eaaa25730
kav04 <51361047+kav04@users.noreply.github.com>
2019-07-28 17:33:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-n,q=map(int,input().split())
+a,b=map(int,input().split())
 m=list(map(int,input().split()))
-count=0
+c=0
 for i in range(len(m)): 
     for j in range(i+1,len(m)): 
-        if(m[i]+m[j]==q): 
-            count=1 
+        if(m[i]+m[j]==b): 
+            c=1 
             break
-if(count): 
+if(c): 
     print("yes")
 else: 
     print("no")
