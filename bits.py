d05f2897dc64f33b6d2b53e4d425954694246647
kav04 <51361047+kav04@users.noreply.github.com>
2019-07-28 19:19:06 +0530

    Create bits.py

---
+++ bits.py
@@ -0,0 +1,14 @@
+np=int(input())
+mp=[]*n
+for i in range(0,2**np): 
+    k=[] 
+    for j in range(np-1,0,-1):
+        k.append(int(i/2**j)%2) 
+    k.append(i%2) 
+    mp.append(k) 
+for i in mp: 
+    s='' 
+    for j in i:
+        j=str(j)
+        s=s+j
+    print(s) 
