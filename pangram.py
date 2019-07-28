34e43c998e732ad0f77333626e62c882a56ef951
kav04 <51361047+kav04@users.noreply.github.com>
2019-07-28 19:26:14 +0530

    Update pangram.py

--- pangram.py
+++ pangram.py
@@ -6,7 +6,8 @@ for i in range(len(n)):
 for i in l: 
     if ord(i)>=65 and ord(i)<=90: 
         l.remove(i) 
-        l.append(i.lower()) d={'a':l.count('a'),'b':l.count('b'),'c':l.count('c'),'d':l.count('d'),'e':l.count('e'),'f':l.count('f'),'g':l.count('g'),'h':l.count('h'),'i':l.count('i'),'j':l.count('j'),'k':l.count('k'),'l':l.count('l'),'m':l.count('m'),'n':l.count('n'),'o':l.count('o'), 'p':l.count('p'),'q':l.count('q'),'r':l.count('r'),'s':l.count('s'),'t':l.count('t'), 'u':l.count('u'),'v':l.count('v'),'w':l.count('w'),'x':l.count('x'),'y':l.count('y'), 'z':l.count('z')}
+        l.append(i.lower()) 
+d={'a':l.count('a'),'b':l.count('b'),'c':l.count('c'),'d':l.count('d'),'e':l.count('e'),'f':l.count('f'),'g':l.count('g'),'h':l.count('h'),'i':l.count('i'),'j':l.count('j'),'k':l.count('k'),'l':l.count('l'),'m':l.count('m'),'n':l.count('n'),'o':l.count('o'), 'p':l.count('p'),'q':l.count('q'),'r':l.count('r'),'s':l.count('s'),'t':l.count('t'), 'u':l.count('u'),'v':l.count('v'),'w':l.count('w'),'x':l.count('x'),'y':l.count('y'), 'z':l.count('z')}
 count=1
 for key,value in d.items():
     if d[key]==0: 
