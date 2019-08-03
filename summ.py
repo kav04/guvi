f684e82e402b97259a188f6d480010ff8bc74da7
kav04 <51361047+kav04@users.noreply.github.com>
2019-08-04 00:17:04 +0530

    Update summ.py

--- summ.py
+++ summ.py
@@ -1,15 +1,15 @@
-def func(ind,s):
-    lst=[] 
+def func(s,ind): 
     sum=0
-    for x in range(0,s):
-        num=input()
-        lst.append(num)
     if (ind<s):
         for i in range(0,ind):
             sum=sum+int(lst[i])
         return sum
-index=int(input())
 size=int(input())
-op=func(index,size)
+lst=[]
+for x in range(0,size):
+    num=int(input()) 
+    lst.append(num)
+index=int(input()) 
+op=func(size,index)
 print(op)
     
