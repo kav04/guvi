04249627be345fd66c6ae1e7be134e70d0daaf17
kav04 <51361047+kav04@users.noreply.github.com>
2019-08-04 20:01:29 +0530

    Update maxi.py

--- maxi.py
+++ maxi.py
@@ -3,4 +3,9 @@ lsr=[]
 for i in range(0,s):
     n=int(input())
     lsr.append(n)
-print(max(lsr))
+max=lsr[0] 
+for j in range(1,s):
+    if(max<lsr[j]):  
+        max=lsr[j]       
+print(max)
+
