a7f134bb0d29d793bbb59b136e7704967fc7dff2
kav04 <51361047+kav04@users.noreply.github.com>
2019-08-06 17:53:38 +0530

    Update power.py

--- code/power.py
+++ code/power.py
@@ -1,10 +1,15 @@
 s=int(input())
 flag=0
-while(s!=0):
-    s=s/2
-    if(s==2):
-        flag=1
-if(flag==1):
+if(s>1):
+    while(s!=0):
+        s=s/2
+        if(s==2):
+            flag=1
+    if(flag==1):
+        print("yes")
+    else:
+        print("no")
+elif(s==1):
     print("yes")
 else:
     print("no")
