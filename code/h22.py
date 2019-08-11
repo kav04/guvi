2c381e21e603d51878ae9ec4be4d51f7e3228841
kav04 <51361047+kav04@users.noreply.github.com>
2019-08-11 18:26:21 +0530

    Update h22.py

--- code/h22.py
+++ code/h22.py
@@ -1,8 +1,10 @@
-import numpy
 n=int(input())
 r=[int(x) for x in input().split()]
 for i in range(0,n):
     a=r[i]
     r.remove(r[i])
-    print(numpy.prod(r),end=" ")
+    prod=1
+    for j in r:
+        prod*=j
+    print(prod,end=" ")
     r.insert(i,a)
