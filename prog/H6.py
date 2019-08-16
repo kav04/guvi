536a54fbd28ba24f440e27a8299db093295aa911
kav04 <51361047+kav04@users.noreply.github.com>
2019-08-16 19:23:07 +0530

    Update H6.py

--- prog/H6.py
+++ prog/H6.py
@@ -1,6 +1,11 @@
-n=int(input())
-l=[int(x) for x in input().split()] 
-for i in range(0,n):
-    if(l.count(l[i])>1):
-        print(l[i])
-        break
+def func():
+    n=int(input())
+    l=[int(x) for x in input().split()]
+    a=[]
+    for i in range(0,n):
+        if(l[i] not in a):
+            a.append(l[i])
+        else:
+            print(l[i])
+            break
+op=func()
