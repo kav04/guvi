75cdfb2a21994dd43ed91841ef0cb0210b6cceab
kav04 <51361047+kav04@users.noreply.github.com>
2019-08-19 21:12:42 +0530

    Update H95.py

--- prog/H95.py
+++ prog/H95.py
@@ -3,11 +3,14 @@ x=n
 a=[]
 while(x!=1):
     count=0
-    for i in range(1,n+1):
+    for i in range(1,n):
         if(x%i==0):
             count+=1
     if(count==2):
         a.append(x)
     x-=1
 a.sort()
-print(*a)
+if(n==2):
+    print(0)
+else:
+    print(*a)
