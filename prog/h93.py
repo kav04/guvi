e0d6a20feb69d2206c47883bf2822e85f3e12822
kav04 <51361047+kav04@users.noreply.github.com>
2019-08-20 07:38:03 +0530

    Update h93.py

--- prog/h93.py
+++ prog/h93.py
@@ -1,9 +1,18 @@
 n=input()
 x=n.split()
+a=[]
+b=[]
 for i in x:
-    for j in range(len(i)):
-        if(j%2==0):
-            print(i[j].upper(),end="")
-        else:
-            print(i[j].lower(),end="")
-    print(end=" ")
+    a.append(len(i))
+z="".join(x)
+for i in range(len(z)):
+    if(i%2==0):
+        b.append(z[i].upper())
+    else:
+        b.append(z[i].lower())
+w="".join(b)
+for k in a:
+    for h in range(0,len(w),k):
+        l=w[h:h+k]
+        print(l,end=" ")
+    break
